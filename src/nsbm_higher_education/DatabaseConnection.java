39987daa27a64bdef951b5709be2c66a64bef9a2
ruwan <ruwan@192.168.8.100>
2018-08-15 09:08:25 -0700

    Adding DatabaseConnection funtion

---
+++ src/nsbm_higher_education/DatabaseConnection.java
@@ -0,0 +1,17 @@
+package nsbm_higher_education;
+import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+//for get database connection
+public class DatabaseConnection {
+    public static Connection getDatabaseConnection(){
+        Connection connection = null;
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/nsbm?zeroDateTimeBehavior=convertToNull","root","");
+        }
+        catch(ClassNotFoundException | SQLException e){
+        }
+        return connection;
+    }
+}
